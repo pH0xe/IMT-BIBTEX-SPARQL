db6b62a9148e3cad74f833dead13d3c0624541ca
Julien Reig <julien.reig6@gmail.com>
2022-05-30 14:25:48 +0200

    Ajout des informations d'expiration du token dans le payload

--- auth-service/jwtHandler.py
+++ auth-service/jwtHandler.py
@@ -1,4 +1,5 @@
 import string
+import time
 import jwt
 import os
 
@@ -8,6 +9,9 @@ import os
 def generateJwtToken(username, isAdmin) -> string:
     payload_data = {
         "username": username,
-        "isAdmin": isAdmin
+        "isAdmin": isAdmin,
+        "iat": int(time.time()),
+        "exp": int(time.time()) + 3600,
+        "nbf": int(time.time()) - 10,
     }
     return jwt.encode(payload_data, os.environ.get('AUTHSECRET'), algorithm="HS256")
